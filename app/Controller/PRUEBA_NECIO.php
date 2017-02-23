80c503f36d576a4c86b467714e98ceae5e5d6a33
donalgn <donalgn@hotmail.com>
2017-02-22 22:04:46 -0600

    22/02/2017_V2

---
+++ app/Controller/PRUEBA_NECIO.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PRUEBA_NECIO
+ *
+ * @author NXT00365
+ */
+class PRUEBA_NECIO {
+    //put your code here
+}
